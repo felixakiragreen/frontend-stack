50cc83298f9b4e2510e6c8ae62dc1586401a75f7
Felix Green <work@felix.green>
2022-12-19 14:55:04 -0500

    add mdsvex

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,15 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite'
+import netlify from '@sveltejs/adapter-netlify'
+
+import { mdsvex } from 'mdsvex'
+import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: netlify(),
+		resolve: {
+			alias: {
+				'@': '/src',
+			},
+		},
+	},
+}
 
-export default config;
+export default config
