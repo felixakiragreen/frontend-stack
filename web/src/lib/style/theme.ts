3360a137c27a70d846f89d5690a8c06b321d642b
Felix Green <work@felix.green>
2022-12-19 15:20:34 -0500

    add style

---
+++ web/src/lib/style/theme.ts
@@ -0,0 +1,540 @@
+import type * as Stitches from '@stitches/core'
+
+const rgba = (hexCode: string, opacity: number) => {
+	let hex = hexCode.replace('#', '')
+
+	if (hex.length === 3) {
+		hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`
+	}
+
+	const r = parseInt(hex.substring(0, 2), 16)
+	const g = parseInt(hex.substring(2, 4), 16)
+	const b = parseInt(hex.substring(4, 6), 16)
+
+	return `rgba(${r},${g},${b},${opacity / 100})`
+}
+
+const color = {
+	felix: '#adff2f',
+	black: '#000000',
+	white: '#ffffff',
+	clear: 'hsla(0, 0%, 0%, 0)',
+	//
+	grey900: '#0a0a09',
+	grey800: '#141411',
+	grey750: '#1d1e1a',
+	grey700: '#2a2b26',
+	grey650: '#373832',
+	grey600: '#53544c',
+	grey500: '#7f8076',
+	grey400: '#aaaba0',
+	grey300: '#d3d4c9',
+	grey200: '#ebebe2',
+	grey100: '#f5f5ef',
+	red900: '#290607',
+	red800: '#470a0d',
+	red700: '#710e15',
+	red600: '#a11420',
+	red500: '#cb1d37',
+	red400: '#ed3467',
+	red300: '#fc64a1',
+	red200: '#ffa3d6',
+	red100: '#ffecfa',
+	orange900: '#2f150d',
+	orange800: '#50240f',
+	orange700: '#75330f',
+	orange600: '#9e490c',
+	orange500: '#cb6709',
+	orange400: '#f1901d',
+	orange300: '#fcb760',
+	orange200: '#fed7a8',
+	orange100: '#fff6e9',
+	yellow900: '#371f05',
+	yellow800: '#5f3906',
+	yellow700: '#8f5c07',
+	yellow600: '#c6930b',
+	yellow500: '#f8cb1b',
+	yellow400: '#f7dd4b',
+	yellow300: '#f5ea7e',
+	yellow200: '#f3f1b0',
+	yellow100: '#f2f2de',
+	green900: '#052907',
+	green800: '#0c4a07',
+	green700: '#1e6e08',
+	green600: '#3f9608',
+	green500: '#6fbf10',
+	green400: '#a5e22c',
+	green300: '#cfed6d',
+	green200: '#e4efab',
+	green100: '#e8ead8',
+	blue900: '#001336',
+	blue800: '#002662',
+	blue700: '#013f8c',
+	blue600: '#0362b2',
+	blue500: '#0f8fd0',
+	blue400: '#38bce6',
+	blue300: '#70dbef',
+	blue200: '#a9eaf4',
+	blue100: '#e5f6f8',
+	purple900: '#120639',
+	purple800: '#271163',
+	purple700: '#442294',
+	purple600: '#643bc0',
+	purple500: '#8659e1',
+	purple400: '#9f7af2',
+	purple300: '#b29af8',
+	purple200: '#ccbdf9',
+	purple100: '#f0e6f9',
+}
+
+const lightThemeColors = {
+	//
+	background: '$white',
+	foreground: '$grey900',
+	//
+	faint: '$grey200',
+	faded: '$grey300',
+	jaded: '$grey400',
+	muted: '$grey500',
+	dusky: '$grey700',
+	//
+	highlight: '$green700',
+	lowlight: '$blue700',
+	//
+	depthIn: '$grey200',
+	depthOut: '$grey300',
+}
+const darkThemeColors = {
+	// THEME dependent tokens
+	//
+	background: '$grey900',
+	foreground: '$grey100',
+	//
+	faint: '$grey800',
+	faded: '$grey700',
+	jaded: '$grey600',
+	muted: '$grey500',
+	dusky: '$grey300',
+	//
+	highlight: '$green300',
+	lowlight: '$blue300',
+	//
+	depthIn: '$grey800',
+	depthOut: '$grey700',
+	// depthIn: rgba(color.white, 5),
+	// depthOut: rgba(color.white, 15),
+}
+
+export const colors = {
+	...color,
+	//
+	felix50: rgba(color.felix, 50),
+	//
+	whiteA25: rgba(color.white, 25),
+	whiteA50: rgba(color.white, 50),
+	whiteA75: rgba(color.white, 75),
+	blackA25: rgba(color.black, 25),
+	blackA50: rgba(color.black, 50),
+	blackA75: rgba(color.black, 75),
+
+	//
+	primary: '$green400',
+	primaryHover: '$green300',
+	primaryFocus: '$green600',
+	primaryFaded: '$green800',
+	primaryText: '$green900',
+
+	// experiments
+	green200A50: rgba(color.green200, 50),
+	green300A50: rgba(color.green300, 50),
+	green400A50: rgba(color.green400, 50),
+	//
+
+	//
+	...darkThemeColors,
+}
+
+const space = {
+	0: '0px', // 0px
+	px: '1px', // 1px
+	'0_5': '0.125rem', // 2px
+	1: '0.25rem', // 4px
+	'1_5': '0.375rem', // 6px
+	2: '0.5rem', // 8px
+	'2_5': '0.625rem', // 10px
+	3: '0.75rem', // 12px
+	'3_5': '0.875rem', // 14px
+	4: '1rem', // 16px
+	5: '1.25rem', // 20px
+	6: '1.5rem', // 24px
+	7: '1.75rem', // 28px
+	8: '2rem', // 32px
+	9: '2.25rem', // 36px
+	10: '2.5rem', // 40px
+	11: '2.75rem', // 44px
+	12: '3rem', // 48px
+	14: '3.5rem', // 56px
+	16: '4rem', // 64px
+	20: '5rem', // 80px
+	24: '6rem', // 96px
+	28: '7rem', // 112px
+	32: '8rem', // 128px
+	36: '9rem', // 144px
+	40: '10rem', // 160px
+	44: '11rem', // 176px
+	48: '12rem', // 192px
+	52: '13rem', // 208px
+	56: '14rem', // 224px
+	60: '15rem', // 240px
+	64: '16rem', // 256px
+	72: '18rem', // 288px
+	80: '20rem', // 320px
+	96: '24rem', // 384px
+	112: '28rem', // 448px
+	128: '32rem', // 512px
+	144: '36rem', // 576px
+	160: '40rem', // 640px
+	'-px': '-1px', // 1px
+	'-0_5': '-0.125rem', // 2px
+	'-1': '-0.25rem', // 4px
+	'-1_5': '-0.375rem', // 6px
+	'-2': '-0.5rem', // 8px
+	'-2_5': '-0.625rem', // 10px
+	'-3': '-0.75rem', // 12px
+	'-3_5': '-0.875rem', // 14px
+	'-4': '-1rem', // 16px
+	'-5': '-1.25rem', // 20px
+	'-6': '-1.5rem', // 24px
+	'-7': '-1.75rem', // 28px
+	'-8': '-2rem', // 32px
+	'-9': '-2.25rem', // 36px
+	'-10': '-2.5rem', // 40px
+	'-11': '-2.75rem', // 44px
+	'-12': '-3rem', // 48px
+	'-14': '-3.5rem', // 56px
+	'-16': '-4rem', // 64px
+	'-20': '-5rem', // 80px
+	'-24': '-6rem', // 96px
+	'-28': '-7rem', // 112px
+	'-32': '-8rem', // 128px
+	'-36': '-9rem', // 144px
+	'-40': '-10rem', // 160px
+	'-44': '-11rem', // 176px
+	'-48': '-12rem', // 192px
+	'-52': '-13rem', // 208px
+	'-56': '-14rem', // 224px
+	'-60': '-15rem', // 240px
+	'-64': '-16rem', // 256px
+	'-72': '-18rem', // 288px
+	'-80': '-20rem', // 320px
+	'-96': '-24rem', // 384px
+	'-112': '-28rem', // 448px
+	'-128': '-32rem', // 512px
+	'-144': '-36rem', // 576px
+	'-160': '-40rem', // 640px
+}
+
+const spacing = {
+	no: space[0],
+	xs: space[1],
+	sm: space[2],
+	md: space[4],
+	lg: space[6],
+	xl: space[8],
+	'2xl': space[10],
+	'3xl': space[12],
+	'4xl': space[16],
+	'5xl': space[20],
+	'6xl': space[24],
+	'7xl': space[28],
+	'8xl': space[32],
+	'9xl': space[36],
+}
+
+// create a util...
+
+const max = {
+	'0': '0rem',
+	none: 'none',
+	xs: '20rem',
+	sm: '24rem',
+	md: '28rem',
+	lg: '32rem',
+	xl: '36rem',
+	'2xl': '42rem',
+	'3xl': '48rem',
+	'4xl': '56rem',
+	'5xl': '64rem',
+	'6xl': '72rem',
+	'7xl': '80rem',
+	full: '100%',
+	min: 'min-content',
+	max: 'max-content',
+	prose: '65ch',
+	'screen-w': '100vw',
+	'screen-h': '100vh',
+	'screen-sm': '640px',
+	'screen-md': '768px',
+	'screen-lg': '1024px',
+	'screen-xl': '1280px',
+	'screen-2xl': '1536px',
+}
+
+// TODO: create util for css={{ scrollMarginTop: '$48' }}
+
+// max-w-0	max-width: 0rem;
+// max-w-none	max-width: none;
+// max-w-xs	max-width: 20rem;
+// max-w-sm	max-width: 24rem;
+// max-w-md	max-width: 28rem;
+// max-w-lg	max-width: 32rem;
+// max-w-xl	max-width: 36rem;
+// max-w-2xl	max-width: 42rem;
+// max-w-3xl	max-width: 48rem;
+// max-w-4xl	max-width: 56rem;
+// max-w-5xl	max-width: 64rem;
+// max-w-6xl	max-width: 72rem;
+// max-w-7xl	max-width: 80rem;
+// max-w-full	max-width: 100%;
+// max-w-min	max-width: min-content;
+// max-w-max	max-width: max-content;
+// max-w-prose	max-width: 65ch;
+// max-w-screen-sm	max-width: 640px;
+// max-w-screen-md	max-width: 768px;
+// max-w-screen-lg	max-width: 1024px;
+// max-w-screen-xl	max-width: 1280px;
+// max-w-screen-2xl	max-width: 1536px;
+
+export const tokens = {
+	colors,
+	space: {
+		...space,
+		...spacing,
+	},
+	sizes: {
+		...space,
+		...max,
+	},
+	fontSizes: {
+		xs: '0.75rem',
+		sm: '0.875rem',
+		md: '1rem',
+		lg: '1.125rem',
+		xl: '1.25rem',
+		'2xl': '1.5rem',
+		'3xl': '1.875rem',
+		'4xl': '2.25rem',
+		'5xl': '3rem',
+		'6xl': '3.75rem',
+		'7xl': '4.5rem',
+		'8xl': '6rem',
+		'9xl': '8rem',
+	},
+	fonts: {
+		main: 'Inter, georgia, apple-system, sans-serif',
+		mono: '"Azeret Mono", menlo, monospace',
+		sans: 'Inter, georgia, apple-system, sans-serif',
+	},
+	fontWeights: {
+		thin: '100',
+		light: '300',
+		base: '400',
+		semi: '500',
+		bold: '700',
+		black: '900',
+	},
+	lineHeights: {
+		xs: '1rem',
+		sm: '1.25rem',
+		md: '1.5rem',
+		lg: '1.75rem',
+		xl: '2rem',
+		'2xl': '2.25rem',
+		'3xl': '2.5rem;',
+		'4xl': '2.75rem',
+		'5xl': '1',
+		'6xl': '1',
+		'7xl': '1',
+		'8xl': '1',
+		'9xl': '1',
+	},
+	//
+	// letterSpacings: {},
+	// sizes: {},
+	borders: {
+		none: '0',
+		muted: '1px solid $colors$muted',
+	},
+	borderWidths: {
+		1: '1px',
+	},
+	borderStyles: {},
+	radii: {
+		none: '0px',
+		sm: '0.125rem',
+		base: '0.25rem',
+		md: '0.375rem',
+		lg: '0.5rem',
+		xl: '0.75rem',
+		'2xl': '1rem',
+		'3xl': '1.5rem',
+		full: '9999px',
+	},
+	shadows: {
+		popup: '0 0 6px 0 rgba(0, 0, 0, 0.67)',
+		sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+		hard: '4px 4px 0px 0 $colors$black',
+		base: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+		md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+		lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+		xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+		'2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+		'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+		inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+		none: 'none',
+	},
+	// zIndices: {},
+	transitions: {
+		1: 'all cubic-bezier(0.4, 0, 0.2, 1) 150ms',
+		2: 'opacity cubic-bezier(0.4, 0, 0.2, 1) 150ms',
+		3: 'all cubic-bezier(0.4, 0, 0.2, 1) 350ms',
+	},
+}
+
+export const utils = {
+	m: (value: Stitches.ScaleValue<'space'>) => ({
+		marginTop: value,
+		marginBottom: value,
+		marginLeft: value,
+		marginRight: value,
+	}),
+	mt: (value: Stitches.ScaleValue<'space'>) => ({
+		marginTop: value,
+	}),
+	mr: (value: Stitches.ScaleValue<'space'>) => ({
+		marginRight: value,
+	}),
+	mb: (value: Stitches.ScaleValue<'space'>) => ({
+		marginBottom: value,
+	}),
+	ml: (value: Stitches.ScaleValue<'space'>) => ({
+		marginLeft: value,
+	}),
+	mx: (value: Stitches.ScaleValue<'space'>) => ({
+		marginLeft: value,
+		marginRight: value,
+	}),
+	my: (value: Stitches.ScaleValue<'space'>) => ({
+		marginTop: value,
+		marginBottom: value,
+	}),
+	p: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingTop: value,
+		paddingBottom: value,
+		paddingLeft: value,
+		paddingRight: value,
+	}),
+	pt: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingTop: value,
+	}),
+	pr: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingRight: value,
+	}),
+	pb: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingBottom: value,
+	}),
+	pl: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingLeft: value,
+	}),
+	px: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingLeft: value,
+		paddingRight: value,
+	}),
+	py: (value: Stitches.ScaleValue<'space'>) => ({
+		paddingTop: value,
+		paddingBottom: value,
+	}),
+	text: (value) => ({
+		fontSize: value,
+		lineHeight: value,
+	}),
+	w: (value: Stitches.ScaleValue<'sizes'>) => ({
+		width: value,
+	}),
+	h: (value: Stitches.ScaleValue<'sizes'>) => ({
+		height: value,
+	}),
+	size: (value: Stitches.ScaleValue<'sizes'>) => ({
+		w: value,
+		h: value,
+	}),
+	surrounding: (value: Stitches.ScaleValue<'space'>) => ({
+		top: value,
+		right: value,
+		bottom: value,
+		left: value,
+	}),
+	s: (value: Stitches.ScaleValue<'space'>) => ({
+		// Unfortunately gap doesn't work on mobile Safari
+		gap: value,
+		// '& > * + *': {
+		// 	ml: value,
+		// },
+	}),
+	sx: (value: Stitches.ScaleValue<'space'>) => ({
+		// Unfortunately gap doesn't work on mobile Safari
+		columnGap: value,
+		// '& > * + *': {
+		// 	ml: value,
+		// },
+	}),
+	sy: (value: Stitches.ScaleValue<'space'>) => ({
+		// Unfortunately gap doesn't work on mobile Safari
+		rowGap: value,
+		// '& > * + *': {
+		// 	mt: value,
+		// },
+	}),
+	backdropFilter: (value) => ({
+		backdropFilter: value,
+		'-webkit-backdrop-filter': value,
+	}),
+	linearGradient: (value) => ({
+		backgroundImage: `linear-gradient(${value})`,
+	}),
+	blur: (value) => ({
+		filter: `blur(${value})`,
+	}),
+	bg: (value: Stitches.PropertyValue<'color'>) => ({
+		backgroundColor: value,
+	}),
+	clip: (value) => {
+		const values = {
+			hexagon:
+				'polygon(50% 0, 93.3% 25%, 93.3% 75%, 50% 100%, 6.7% 75%, 6.7% 25%)',
+		}
+
+		return {
+			clipPath: values[value] || value,
+		}
+	},
+	b: (value: Stitches.PropertyValue<'color'>) => {
+		// TODO: rework
+		// const [value1, value2] = value.split('-')
+		// `${}-${
+		// type BorderValue = Stitches.PropertyValue<'color'>
+
+		return {
+			border: `1px solid ${value}`,
+		}
+	},
+	truncate: () => ({
+		overflow: 'hidden',
+		whiteSpace: 'nowrap',
+		textOverflow: 'ellipsis',
+	}),
+}
+
+// --tw-space-y-reverse: 0;
+// margin-top: calc(0px * calc(1 - var(--tw-space-y-reverse)));
+// margin-bottom: calc(0px * var(--tw-space-y-reverse));
