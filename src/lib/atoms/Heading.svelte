cc2b2311cbeb363a4cab11af3a0fb5e4600d9004
Felix Green <work@felix.green>
2023-05-16 20:48:32 -0400

    Update Heading.svelte

--- src/lib/atoms/Heading.svelte
+++ src/lib/atoms/Heading.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { stitch } from '@/ui'
-	import type { CSS } from '@/ui'
+	import type { PropCss, PropString, PropComponent } from '@/types'
 
-	export let id = null
-	export let cls = null
-	export let css: CSS = null
-	export let style = null
+	export let id: PropString = undefined
+	export let cls: PropComponent = undefined
+	export let css: PropCss = undefined
+	export let style: PropString = undefined
 	export let vrt = {}
 
 	export let h = 1
@@ -14,27 +14,27 @@
 </script>
 
 {#if h === 6}
-	<h6 class={ss({ ...vrt, css })} {style} {id}>
+	<h6 {id} class={ss({ ...vrt, css })} {style}>
 		<slot />
 	</h6>
 {:else if h === 5}
-	<h5 class={ss({ ...vrt, css })} {style} {id}>
+	<h5 {id} class={ss({ ...vrt, css })} {style}>
 		<slot />
 	</h5>
 {:else if h === 4}
-	<h4 class={ss({ ...vrt, css })} {style} {id}>
+	<h4 {id} class={ss({ ...vrt, css })} {style}>
 		<slot />
 	</h4>
 {:else if h === 3}
-	<h3 class={ss({ ...vrt, css })} {style} {id}>
+	<h3 {id} class={ss({ ...vrt, css })} {style}>
 		<slot />
 	</h3>
 {:else if h === 2}
-	<h2 class={ss({ ...vrt, css })} {style} {id}>
+	<h2 {id} class={ss({ ...vrt, css })} {style}>
 		<slot />
 	</h2>
 {:else}
-	<h1 class={ss({ ...vrt, css })} {style} {id}>
+	<h1 {id} class={ss({ ...vrt, css })} {style}>
 		<slot />
 	</h1>
 {/if}
