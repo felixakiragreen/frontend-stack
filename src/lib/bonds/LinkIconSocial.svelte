5ab24ae729802fcd057698b2c77e7c4d629576b7
Felix Green <work@felix.green>
2023-05-16 20:48:45 -0400

    Update LinkIconSocial.svelte

--- src/lib/bonds/LinkIconSocial.svelte
+++ src/lib/bonds/LinkIconSocial.svelte
@@ -1,28 +1,42 @@
 <script lang="ts">
-	import { stitch } from '@/ui'
+	import {
+		IconTheme,
+		type PropBoolean,
+		type PropString,
+		type SocialLink,
+	} from '@/types'
+
 	import Link from './Link.svelte'
 	import IconSocial from './IconSocial.svelte'
-	import Square from './SvgSquare.svelte'
 
-	export let url
-	export let newTab = true
-	export let color
-	export let path
-	export let transform
+	export let url: string
+	export let newTab = false
 
-	// variants
-	export let symbol = null
-	export let ground = null
-	export let hoverSymbol = null
-	export let hoverGround = null
+	export let brandHue: PropString = undefined
+	export let brandExact: PropString = undefined
+	export let path: PropString = undefined
+	export let transform: SocialLink['transform']
+
+	export let shape: 'square' | 'hexagon' = 'square'
+	export let rounded: PropBoolean = undefined
+
+	export let symbol: IconTheme | undefined = IconTheme.Foreground
+	export let ground: IconTheme | undefined = IconTheme.Background
+	export let hoverSymbol: IconTheme | undefined = undefined
+	export let hoverGround: IconTheme | undefined = undefined
 </script>
 
 <Link {url} {newTab} look="clean">
 	<IconSocial
-		brandHue={color.hue}
-		brandExact={color.exact}
-		{...{ symbol, ground, hoverSymbol, hoverGround }}
+		{shape}
+		{rounded}
 		{path}
 		{transform}
+		{symbol}
+		{ground}
+		{hoverSymbol}
+		{hoverGround}
+		{brandHue}
+		{brandExact}
 	/>
 </Link>
