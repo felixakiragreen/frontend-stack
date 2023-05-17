79b892e93f4b4c79f9883d5146eefed5dd67b0af
Felix Green <work@felix.green>
2023-05-16 20:49:01 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
-import type { CSS } from '@/ui'
-export type { CSS }
+import type { CSS } from './stitches.config'
+import type * as Stitches from '@stitches/core'
+import type { CssComponent } from '@stitches/core/types/styled-component'
+
+export type { CSS, CssComponent, Stitches }
 
 export const baseColors = [
 	'red',
@@ -10,4 +13,47 @@ export const baseColors = [
 	'purple',
 	'grey',
 ] as const
-export type Color = typeof baseColors[number]
+export type Color = (typeof baseColors)[number]
+
+export enum IconTheme {
+	None = 'no',
+	Clear = 'cl',
+	CurrentColor = 'cc',
+	Foreground = 'fg',
+	Background = 'bg',
+	Highlight = 'hl',
+	Lowlight = 'll',
+	BrandExact = 'be',
+	BrandHue = 'bh',
+}
+
+export type PropString = string | null | undefined
+export type PropBoolean = boolean | null | undefined
+export type PropComponent = CssComponent | null | undefined
+export type PropCss = CSS | undefined
+
+export type VariantOption<
+	Component extends { [key: symbol | string]: any },
+	VariantName extends
+		| keyof Stitches.VariantProps<Component>
+		| unknown = unknown,
+> = VariantName extends keyof Stitches.VariantProps<Component>
+	? Extract<Stitches.VariantProps<Component>[VariantName], string>
+	: Extract<Stitches.VariantProps<Component>, string>
+
+export type SocialLink = {
+	url: string
+	username: string
+	title: string
+	hex: string
+	hue: string
+	path: string
+	transform: {
+		scale: number
+		translate: {
+			x: number
+			y: number
+		}
+	}
+	look?: string
+}
