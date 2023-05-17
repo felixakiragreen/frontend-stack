dea385dfa4417ac42e306513d3675c98e7a518a2
Felix Green <work@felix.green>
2023-05-16 20:49:00 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Span } from '@/lib/atoms'
-	import { Content, Stack, Inline, Text, Break, Grid } from '@/lib/bonds'
+	import { Content, VStack, HStack, Text, Break } from '@/lib/bonds'
 </script>
 
 <Content
@@ -8,11 +8,11 @@
 		bg: '$orange700',
 	}}
 >
-	<Stack
+	<VStack
 		css={{
 			bg: '$orange600',
 		}}
 	>
 		<Text>Body</Text>
-	</Stack>
+	</VStack>
 </Content>
