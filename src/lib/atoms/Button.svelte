f9fd1aa3d3937fe3780a8a6362bb00f1b78eb4f2
Felix Green <work@felix.green>
2023-05-16 20:48:31 -0400

    Update Button.svelte

--- src/lib/atoms/Button.svelte
+++ src/lib/atoms/Button.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import { stitch } from '@/ui'
-	import type { CSS } from '@/ui'
+	import type { PropCss, PropString, PropComponent, PropBoolean } from '@/types'
 
-	export let id = null
-	export let cls = null
-	export let css: CSS = null
-	export let style = null
+	export let id: PropString = undefined
+	export let cls: PropComponent = undefined
+	export let css: PropCss = undefined
+	export let style: PropString = undefined
 	export let vrt = {}
 
-	export let disabled = null
+	export let disabled: PropBoolean = null
 
 	const ss = cls || stitch({})
 </script>
 
-<button class={ss({ ...vrt, css })} {style} {id} {disabled} on:click>
+<button {id} class={ss({ ...vrt, css })} {style} {disabled} on:click>
 	<slot />
 </button>
