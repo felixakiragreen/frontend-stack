e0acaa089881f447dd0f1034e2cccb3847feeb02
Felix Green <work@felix.green>
2023-05-23 19:46:14 -0400

    cleanup breakpoints

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -16,15 +16,6 @@ export const {
 	theme: {
 		...tokens,
 	},
-	// media: {
-	// 	xxs: '(min-width: 0px)',
-	// 	xs: '(min-width: 320px)',
-	// 	sm: '(min-width: 640px)',
-	// 	md: '(min-width: 768px)',
-	// 	lg: '(min-width: 1024px)',
-	// 	xl: '(min-width: 1280px)',
-	// 	xxl: '(min-width: 1536px)',
-	// },
 	media: {
 		xxs: '(min-width: 240px)',
 		xs: '(min-width: 320px)',
