cbfc8f3e441048630a7db010b4549905d67c53e8
Felix Green <work@felix.green>
2023-05-26 22:41:41 -0400

    add theme page

---
+++ src/routes/theme/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Box, Span } from '@/atoms'
+	import { Content, VStack, HStack, Text, Break } from '@/bonds'
+</script>
+
+<Content
+	css={{
+		bg: '$orange700',
+	}}
+>
+	<VStack
+		css={{
+			bg: '$orange600',
+		}}
+	>
+		<Text>Theme</Text>
+
+		<Box
+			css={{
+				bg: '$green500',
+				height: '$1',
+				width: '$1',
+			}}
+		/>
+	</VStack>
+</Content>
