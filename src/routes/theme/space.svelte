c8be0d05fd8b786165ef72fcf7496aa94fc49ea6
Felix Green <work@felix.green>
2023-05-28 22:42:45 -0400

    add space

---
+++ src/routes/theme/space.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Box, Span } from '@/atoms'
+	import { Content, VStack, HStack, Text, Break } from '@/bonds'
+</script>
+
+<VStack
+	css={{
+		bg: '$orange600',
+	}}
+>
+	<Text>Space</Text>
+</VStack>
