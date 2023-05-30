aab4ae185600079ab8cc4de947005293bfc5a49f
Felix Green <work@felix.green>
2023-05-30 13:24:56 -0400

    remove useless aliases

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,6 @@ const config = {
 	kit: {
 		adapter: netlify(),
 		alias: {
-			'@/atoms': '/src/lib/atoms',
-			'@/bonds': '/src/lib/bonds',
-			'@/cells': '/src/lib/cells',
 			'@': '/src',
 		},
 	},
