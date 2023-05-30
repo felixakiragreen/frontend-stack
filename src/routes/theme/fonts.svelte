2759151242026d8d11a2d8ad7092da3840ef10ab
Felix Green <work@felix.green>
2023-05-29 23:17:17 -0400

    display the fonts

---
+++ src/routes/theme/fonts.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Box, Span } from '@/atoms'
+	import { Content, VStack, HStack, Text, Break } from '@/bonds'
+
+	// # array from 1 to 9
+	const length = 9
+	const array = Array.from({ length }, (_, i) => i + 1)
+</script>
+
+<VStack
+	css={{
+		bg: '$orange600',
+	}}
+>
+	<Text>Fonts</Text>
+
+	{#each array as a}
+		<Text css={{ fontSize: `$${a}` }}>{a}</Text>
+	{/each}
+</VStack>
