77ca4b5c5994bd1e6d224b0a44f4f981076f0e35
Felix Green <work@felix.green>
2023-05-29 23:44:18 -0400

    add the font aliases

--- src/routes/theme/fonts.svelte
+++ src/routes/theme/fonts.svelte
@@ -2,6 +2,8 @@
 	import { Box, Span } from '@/atoms'
 	import { Content, VStack, HStack, Text, Break } from '@/bonds'
 
+	import { fluidFontAliases } from '@/lib/style/fluid'
+
 	// # array from 1 to 9
 	const length = 9
 	const array = Array.from({ length }, (_, i) => i + 1)
@@ -15,6 +17,11 @@
 	<Text>Fonts</Text>
 
 	{#each array as a}
-		<Text css={{ fontSize: `$${a}` }}>{a}</Text>
+		<Text css={{ fontSize: `$${a}` }}>
+			{a} -
+			{fluidFontAliases.shirt.keys[a]}
+			{fluidFontAliases.typo.keys[a]}
+			{fluidFontAliases.step.keys[a]}
+		</Text>
 	{/each}
 </VStack>
