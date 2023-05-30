8f47a494307aa56e7c5ea95d4e42381c164497a1
Felix Green <work@felix.green>
2023-05-30 14:21:42 -0400

    cleanup negatives

--- src/fui/fluid/utils.ts
+++ src/fui/fluid/utils.ts
@@ -1,18 +1,36 @@
 export function addKeys<T extends object>(
 	obj: T,
 ): T & { keys: (string | null)[] } {
-	let keys: (string | null)[] = Object.keys(obj)
-	keys.unshift(null)
+	let keys: string[] = Object.keys(obj)
+	if (keys.length === 9) {
+		keys.unshift('no')
+	}
 	let newObj = { ...obj, keys }
 
 	return newObj as T & { keys: (string | null)[] }
 }
 
-export function addNegatives(obj: any): any {
-	let newObj = { ...obj }
+// This function works in two steps:
+function convertToNegative(str: string): string {
+	return str.replace(/(-?\d*\.?\d+)/g, (match) => {
+		return String(-1 * parseFloat(match))
+	})
+}
+
+function convertClampToNegative(str: string): string {
+	return str.replace(/clamp\((.*?),(.*?),(.*)\)/g, (match, min, val, max) => {
+		let negativeMin = convertToNegative(min.trim())
+		let negativeMax = convertToNegative(max.trim())
+		let negativeVal = convertToNegative(val.trim())
+		return `clamp(${negativeMax}, ${negativeVal}, ${negativeMin})`
+	})
+}
+
+export function addNegatives<T extends Record<string, string>>(obj: T): T {
+	let newObj: Record<string, string> = { ...obj }
 	for (let key in obj) {
-		newObj[`-${key}`] = obj[key]
+		newObj[`-${key}`] = convertClampToNegative(obj[key])
 	}
 
-	return newObj
+	return newObj as T
 }
