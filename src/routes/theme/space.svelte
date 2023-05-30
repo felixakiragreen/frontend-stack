2d679e9b4ef085fab09c10d948bd5ee09df712ab
Felix Green <work@felix.green>
2023-05-30 14:22:44 -0400

    add all the spaces

--- src/routes/theme/space.svelte
+++ src/routes/theme/space.svelte
@@ -1,12 +1,49 @@
 <script lang="ts">
-	import { Box, Span } from '@/atoms'
-	import { Content, VStack, HStack, Text, Break } from '@/bonds'
+	import { Box, Content, VStack, HStack, Text, Break } from '@/fui'
+
+	import { fluidSizeAliases, fluidSpaceAliases } from '@/fui/fluid'
+
+	// array from 1 to 9
+	const length = 10
+	const array = Array.from({ length }, (_, i) => i)
 </script>
 
 <VStack
+	gap="sm"
 	css={{
 		bg: '$orange600',
 	}}
 >
 	<Text>Space</Text>
+
+	{#each array as a}
+		<HStack gap="sm">
+			<Box
+				css={{
+					bg: '$green600',
+					size: `$space$${a}`,
+				}}
+			/>
+			<Text>
+				{a} -
+				{fluidSpaceAliases.shirt.keys[a]}
+			</Text>
+		</HStack>
+	{/each}
+
+	<Text>Sizes</Text>
+
+	{#each array as a}
+		<Box
+			css={{
+				bg: '$green600',
+				size: `$${a}`,
+			}}
+		>
+			<Text>
+				{a} -
+				{fluidSizeAliases.shirt.keys[a]}
+			</Text>
+		</Box>
+	{/each}
 </VStack>
