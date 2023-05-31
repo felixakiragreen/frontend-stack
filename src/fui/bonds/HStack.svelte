16e2a39e1b8cabd8d392aa078991a1a2f9e36f40
Felix Green <work@felix.green>
2023-05-31 13:59:40 -0400

    add properties

--- src/fui/bonds/HStack.svelte
+++ src/fui/bonds/HStack.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
-	import { stitch, Box } from '@/fui'
-	import type { PropCss, PropString, VariantOption } from '@/types'
+	import { stitch, BoxAtom } from '@/fui'
+	import type { PropCss, PropString, VariantOption } from '@/fui'
+	import { generateSpaceVariants } from '@/fui/style/variants'
+	import { stitching } from './Box.svelte'
 
 	export let id: PropString = undefined
 	export let css: PropCss = undefined
-	export let pad: VariantOption<typeof ss, 'pad'> | undefined = undefined
+
+	// padding
+	export let p: VariantOption<typeof ss, 'p'> | undefined = undefined
+	export let px: VariantOption<typeof ss, 'px'> | undefined = undefined
+	export let py: VariantOption<typeof ss, 'py'> | undefined = undefined
+	export let pt: VariantOption<typeof ss, 'pt'> | undefined = undefined
+	export let pr: VariantOption<typeof ss, 'pr'> | undefined = undefined
+	export let pb: VariantOption<typeof ss, 'pb'> | undefined = undefined
+	export let pl: VariantOption<typeof ss, 'pl'> | undefined = undefined
+	// margin
+	export let m: VariantOption<typeof ss, 'm'> | undefined = undefined
+	export let mx: VariantOption<typeof ss, 'mx'> | undefined = undefined
+	export let my: VariantOption<typeof ss, 'my'> | undefined = undefined
+	export let mt: VariantOption<typeof ss, 'mt'> | undefined = undefined
+	export let mr: VariantOption<typeof ss, 'mr'> | undefined = undefined
+	export let mb: VariantOption<typeof ss, 'mb'> | undefined = undefined
+	export let ml: VariantOption<typeof ss, 'ml'> | undefined = undefined
+	// color
+	export let bg: VariantOption<typeof ss, 'bg'> | undefined = undefined
+	export let fg: VariantOption<typeof ss, 'fg'> | undefined = undefined
+	// gap, gapV, align, alignV, collapse
 	export let gap: VariantOption<typeof ss, 'gap'> | undefined = undefined
 	export let gapV: VariantOption<typeof ss, 'gapV'> | undefined = undefined
 	export let align: VariantOption<typeof ss, 'align'> | undefined = undefined
@@ -12,145 +34,12 @@
 	export let collapse: VariantOption<typeof ss, 'collapse'> | undefined =
 		undefined
 
-	const ss = stitch({
+	const ss = stitch(stitching, {
 		display: 'flex',
 
-		// sx: '$$SpaceX',
-		// sy: '$space$no',
-
 		variants: {
-			pad: {
-				no: {
-					p: '$no',
-				},
-				xs: {
-					p: '$xs',
-				},
-				sm: {
-					p: '$sm',
-				},
-				md: {
-					p: '$md',
-				},
-				lg: {
-					p: '$lg',
-				},
-				xl: {
-					p: '$xl',
-				},
-				'2xl': {
-					p: '$2xl',
-				},
-				'3xl': {
-					p: '$3xl',
-				},
-				'4xl': {
-					p: '$4xl',
-				},
-				'5xl': {
-					p: '$5xl',
-				},
-				'6xl': {
-					p: '$6xl',
-				},
-				'7xl': {
-					p: '$7xl',
-				},
-				'8xl': {
-					p: '$8xl',
-				},
-				'9xl': {
-					p: '$9xl',
-				},
-			},
-			gap: {
-				no: {
-					sx: '$no',
-				},
-				xs: {
-					sx: '$xs',
-				},
-				sm: {
-					sx: '$sm',
-				},
-				md: {
-					sx: '$md',
-				},
-				lg: {
-					sx: '$lg',
-				},
-				xl: {
-					sx: '$xl',
-				},
-				'2xl': {
-					sx: '$2xl',
-				},
-				'3xl': {
-					sx: '$3xl',
-				},
-				'4xl': {
-					sx: '$4xl',
-				},
-				'5xl': {
-					sx: '$5xl',
-				},
-				'6xl': {
-					sx: '$6xl',
-				},
-				'7xl': {
-					sx: '$7xl',
-				},
-				'8xl': {
-					sx: '$8xl',
-				},
-				'9xl': {
-					sx: '$9xl',
-				},
-			},
-			gapV: {
-				no: {
-					sy: '$no',
-				},
-				xs: {
-					sy: '$xs',
-				},
-				sm: {
-					sy: '$sm',
-				},
-				md: {
-					sy: '$md',
-				},
-				lg: {
-					sy: '$lg',
-				},
-				xl: {
-					sy: '$xl',
-				},
-				'2xl': {
-					sy: '$2xl',
-				},
-				'3xl': {
-					sy: '$3xl',
-				},
-				'4xl': {
-					sy: '$4xl',
-				},
-				'5xl': {
-					sy: '$5xl',
-				},
-				'6xl': {
-					sy: '$6xl',
-				},
-				'7xl': {
-					sy: '$7xl',
-				},
-				'8xl': {
-					sy: '$8xl',
-				},
-				'9xl': {
-					sy: '$9xl',
-				},
-			},
+			gap: generateSpaceVariants('sx'),
+			gapV: generateSpaceVariants('sy'),
 			align: {
 				leading: {
 					justifyContent: 'flex-start',
@@ -203,6 +92,36 @@
 	})
 </script>
 
-<Box {id} cls={ss} vrt={{ pad, gap, gapV, align, alignV, collapse }} {css}>
+<BoxAtom
+	{id}
+	cls={ss}
+	vrt={{
+		p,
+		px,
+		py,
+		pt,
+		pb,
+		pl,
+		pr,
+		//
+		m,
+		mx,
+		my,
+		mt,
+		mb,
+		ml,
+		mr,
+		//
+		bg,
+		fg,
+		//
+		gap,
+		gapV,
+		align,
+		alignV,
+		collapse,
+	}}
+	{css}
+>
 	<slot />
-</Box>
+</BoxAtom>
