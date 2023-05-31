f553fd97e65a6bd460c6feedf359ae058ce16b95
Felix Green <work@felix.green>
2023-05-31 14:00:00 -0400

    add props

--- src/fui/bonds/VStack.svelte
+++ src/fui/bonds/VStack.svelte
@@ -1,108 +1,42 @@
 <script lang="ts">
-	import { stitch } from '@/fui'
-	import type { PropCss, PropString, VariantOption } from '@/types'
-	import { Box } from '@/fui/atoms'
+	import { stitch, BoxAtom } from '@/fui'
+	import type { PropCss, PropString, VariantOption } from '@/fui'
+	import { generateSpaceVariants } from '@/fui/style/variants'
+	import { stitching } from './Box.svelte'
 
 	export let id: PropString = undefined
 	export let css: PropCss = undefined
-	export let pad: VariantOption<typeof ss, 'pad'> | undefined = undefined
+
+	// padding
+	export let p: VariantOption<typeof ss, 'p'> | undefined = undefined
+	export let px: VariantOption<typeof ss, 'px'> | undefined = undefined
+	export let py: VariantOption<typeof ss, 'py'> | undefined = undefined
+	export let pt: VariantOption<typeof ss, 'pt'> | undefined = undefined
+	export let pr: VariantOption<typeof ss, 'pr'> | undefined = undefined
+	export let pb: VariantOption<typeof ss, 'pb'> | undefined = undefined
+	export let pl: VariantOption<typeof ss, 'pl'> | undefined = undefined
+	// margin
+	export let m: VariantOption<typeof ss, 'm'> | undefined = undefined
+	export let mx: VariantOption<typeof ss, 'mx'> | undefined = undefined
+	export let my: VariantOption<typeof ss, 'my'> | undefined = undefined
+	export let mt: VariantOption<typeof ss, 'mt'> | undefined = undefined
+	export let mr: VariantOption<typeof ss, 'mr'> | undefined = undefined
+	export let mb: VariantOption<typeof ss, 'mb'> | undefined = undefined
+	export let ml: VariantOption<typeof ss, 'ml'> | undefined = undefined
+	// color
+	export let bg: VariantOption<typeof ss, 'bg'> | undefined = undefined
+	export let fg: VariantOption<typeof ss, 'fg'> | undefined = undefined
+	//
 	export let gap: VariantOption<typeof ss, 'gap'> | undefined = undefined
 	export let align: VariantOption<typeof ss, 'align'> | undefined = undefined
 	export let alignV: VariantOption<typeof ss, 'alignV'> | undefined = undefined
 
-	const ss = stitch({
+	const ss = stitch(stitching, {
 		display: 'flex',
 		flexDirection: 'column',
 
 		variants: {
-			pad: {
-				no: {
-					p: '$no',
-				},
-				xs: {
-					p: '$xs',
-				},
-				sm: {
-					p: '$sm',
-				},
-				md: {
-					p: '$md',
-				},
-				lg: {
-					p: '$lg',
-				},
-				xl: {
-					p: '$xl',
-				},
-				'2xl': {
-					p: '$2xl',
-				},
-				'3xl': {
-					p: '$3xl',
-				},
-				'4xl': {
-					p: '$4xl',
-				},
-				'5xl': {
-					p: '$5xl',
-				},
-				'6xl': {
-					p: '$6xl',
-				},
-				'7xl': {
-					p: '$7xl',
-				},
-				'8xl': {
-					p: '$8xl',
-				},
-				'9xl': {
-					p: '$9xl',
-				},
-			},
-			gap: {
-				no: {
-					sy: '$no',
-				},
-				xs: {
-					sy: '$xs',
-				},
-				sm: {
-					sy: '$sm',
-				},
-				md: {
-					sy: '$md',
-				},
-				lg: {
-					sy: '$lg',
-				},
-				xl: {
-					sy: '$xl',
-				},
-				'2xl': {
-					sy: '$2xl',
-				},
-				'3xl': {
-					sy: '$3xl',
-				},
-				'4xl': {
-					sy: '$4xl',
-				},
-				'5xl': {
-					sy: '$5xl',
-				},
-				'6xl': {
-					sy: '$6xl',
-				},
-				'7xl': {
-					sy: '$7xl',
-				},
-				'8xl': {
-					sy: '$8xl',
-				},
-				'9xl': {
-					sy: '$9xl',
-				},
-			},
+			gap: generateSpaceVariants('sy'),
 			align: {
 				leading: {
 					alignItems: 'flex-start',
@@ -130,12 +64,36 @@
 			},
 		},
 	})
-
-	// ss({ css })
-
-	// console.log({ css }, ss({ color, css }))
 </script>
 
-<Box cls={ss} vrt={{ pad, gap, align, alignV }} {css} {id}>
+<BoxAtom
+	cls={ss}
+	vrt={{
+		p,
+		px,
+		py,
+		pt,
+		pb,
+		pl,
+		pr,
+		//
+		m,
+		mx,
+		my,
+		mt,
+		mb,
+		ml,
+		mr,
+		//
+		bg,
+		fg,
+		//
+		gap,
+		align,
+		alignV,
+	}}
+	{css}
+	{id}
+>
 	<slot />
-</Box>
+</BoxAtom>
