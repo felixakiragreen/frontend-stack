39d38a3c137b843cd6b0c1be3af43b2c85cc2348
Felix Green <work@felix.green>
2023-06-01 13:47:47 -0400

    update token name

--- src/fui/style/variants.ts
+++ src/fui/style/variants.ts
@@ -4,24 +4,24 @@ import { theme } from '../stitches.config'
 // Useful Reference: https://konstantin.digital/blog/generating-stitches-variants-for-theme-colors
 
 export function generateSpaceVariants(property: string) {
-	return Object.values(theme.space).reduce((prev, curr) => {
+	return Object.values(theme.space).reduce((acc, next) => {
 		return {
 			//
-			...prev,
-			[curr.token]: {
-				[property]: `$${curr.token}`,
+			...acc,
+			[next.token]: {
+				[property]: `$${next.token}`,
 			},
 		}
 	}, {}) as Record<keyof typeof theme.space, CSS>
 }
 
 export function generateColorVariants(property: string) {
-	return Object.values(theme.colors).reduce((prev, curr) => {
+	return Object.values(theme.colors).reduce((acc, next) => {
 		return {
 			//
-			...prev,
-			[curr.token]: {
-				[property]: `$${curr.token}`,
+			...acc,
+			[next.token]: {
+				[property]: `$${next.token}`,
 			},
 		}
 	}, {}) as Record<keyof typeof theme.colors, CSS>
