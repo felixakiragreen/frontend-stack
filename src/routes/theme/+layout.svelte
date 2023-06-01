26c00016d5d13cce7285d352ec2004d7268d422d
Felix Green <work@felix.green>
2023-06-01 13:48:06 -0400

    add header to theme

---
+++ src/routes/theme/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Box, Content, VStack, HStack, Text, Break, LinkNav } from '@/fui'
+</script>
+
+<Content>
+	<HStack bg="grey800" gap="xl">
+		<LinkNav route="/theme" label="Theme" />
+		<LinkNav route="/theme/atoms" label="Atoms" />
+		<LinkNav route="/theme/space" label="Space" />
+		<LinkNav route="/theme/fonts" label="Fonts" />
+		<LinkNav route="/theme/color" label="Color" />
+	</HStack>
+	<slot />
+</Content>
