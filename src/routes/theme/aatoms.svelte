e70070e1f205ed781b048ff78dd5087b4799fdc0
Felix Green <work@felix.green>
2023-06-06 11:48:23 -0400

    add size & rounding

--- src/routes/theme/aatoms.svelte
+++ src/routes/theme/aatoms.svelte
@@ -14,14 +14,26 @@
 <VStack gap="md" bg="grey900">
 	<Text>Atoms are cool</Text>
 	<VStack gap="md" align="baseline">
-		<HStack gap="md" alignV="baseline">
-			<!-- <Box> -->
-			<Button look="holo" size="no">Holo Button</Button>
-			<Button look="holo" size="xs">Holo Button</Button>
-			<Button look="holo" size="sm">Holo Button</Button>
-			<Button look="holo" size="md">Holo Button</Button>
-			<Button look="holo" size="lg">Holo Button</Button>
-			<!-- </Box> -->
+		<Text>Size</Text>
+		<HStack gap="md" gapV="md" alignV="bottom" wrap>
+			<Button look="holo" size="no">NO</Button>
+			<Button look="holo" size="xs">XS</Button>
+			<Button look="holo" size="sm">SM</Button>
+			<Button look="holo" size="md">MD</Button>
+			<Button look="holo" size="lg">LG</Button>
+			<Button look="holo" size="xl">XL</Button>
+		</HStack>
+		<Text>Rounding</Text>
+		<HStack gap="md" gapV="md" alignV="bottom" wrap>
+			<Button look="holo" size="md" rounding="no">NO</Button>
+			<Button look="holo" size="md" rounding="xs">XS</Button>
+			<Button look="holo" size="md" rounding="sm">SM</Button>
+			<Button look="holo" size="md" rounding="md">MD</Button>
+			<Button look="holo" size="md" rounding="lg">LG</Button>
+			<Button look="holo" size="md" rounding="xl">XL</Button>
+			<Button look="holo" size="xl" rounding="2xl">2XL</Button>
+			<Button look="holo" size="xl" rounding="3xl">3XL</Button>
+			<Button look="holo" size="xl" rounding="full">FULL</Button>
 		</HStack>
 		<!-- <Button look="holoPink">Holo Button</Button>
 		<Button look="holoRed">Holo Button</Button>
