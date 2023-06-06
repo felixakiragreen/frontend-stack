f8a57de3f7b72ac2ceb7b5a72fd68a9e6ba0b240
Felix Green <work@felix.green>
2023-06-06 11:38:13 -0400

    rename components

---
+++ src/routes/theme/aatoms.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import {
+		Box,
+		Span,
+		Content,
+		Button,
+		VStack,
+		HStack,
+		Text,
+		Break,
+	} from '@/fui'
+</script>
+
+<VStack gap="md" bg="grey900">
+	<Text>Atoms are cool</Text>
+	<VStack gap="md" align="baseline">
+		<HStack gap="md" alignV="baseline">
+			<!-- <Box> -->
+			<Button look="holo" size="no">Holo Button</Button>
+			<Button look="holo" size="xs">Holo Button</Button>
+			<Button look="holo" size="sm">Holo Button</Button>
+			<Button look="holo" size="md">Holo Button</Button>
+			<Button look="holo" size="lg">Holo Button</Button>
+			<!-- </Box> -->
+		</HStack>
+		<!-- <Button look="holoPink">Holo Button</Button>
+		<Button look="holoRed">Holo Button</Button>
+		<Button look="purps">Holo Button</Button>
+		<Button look="greens">Holo Button</Button>
+		<Button look="clean">Holo Button</Button> -->
+	</VStack>
+</VStack>
